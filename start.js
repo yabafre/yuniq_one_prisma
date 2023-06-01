edcb483ad03f7938c6291a27e86a7d622ea29ff1
Frédéric Yaba <frederic@bobochicparIs.com>
2023-06-01 16:16:07 +0200

    [SERVER]+- update(update start)

--- start.js
+++ start.js
@@ -6,25 +6,14 @@ const start = promisify(pm2.start.bind(pm2));
 const list = promisify(pm2.list.bind(pm2));
 
 connect()
-    .then(() => start('ecosystem.config.js'))
+    .then(() => start('server/server.js'))
     .then(() => list())
-    .then(processes => {
-        const yuniqApi = processes.find(p => p.name === 'yuniq-api');
+    .then((list) => {
+        console.log(list);
+        pm2.disconnect();
+    } )
+    .catch((err) => {
+        console.log(err);
+        pm2.disconnect();
+    } )
 
-        if (!yuniqApi) {
-            throw new Error('yuniq-api not found in PM2 processes');
-        }
-
-        process.on('SIGINT', () => {
-            pm2.delete('yuniq-api', () => {
-                pm2.disconnect();
-                process.exit();
-            });
-        });
-
-        setInterval(() => {}, 1000);
-    })
-    .catch(err => {
-        console.error('Error:', err);
-        process.exit(1);
-    });
