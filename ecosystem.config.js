25573bd06cdb2d008601ab42b59cd653c3facac6
Frédéric Yaba <frederic@bobochicparIs.com>
2023-06-01 16:10:12 +0200

    [SERVER]+- update(update ecosystem.config)

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,12 @@
-module.exports = [{
-    script: 'server/server.js',
-    name: 'yuniq-api',
-    exec_mode: 'cluster',
-    instances: 1
-}]
+module.exports = {
+    apps: [{
+        script: 'server/server.js',
+        name: 'yuniq-api',
+        exec_mode: 'cluster',
+        instances: 2,
+        out_file: 'NULL', // Redirects stdout (regular logs) to the default output
+        error_file: 'NULL', // Redirects stderr (error logs) to the default output
+        log_date_format: 'YYYY-MM-DD HH:mm Z', // Formats the timestamp
+        combine_logs: true, // Combines stdout and stderr into the same stream
+    }]
+}
