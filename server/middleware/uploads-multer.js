ff47358ca22c60d84ce67cb161604771f0cb52f1
Frédéric Yaba <frederic@bobochicparIs.com>
2023-06-01 22:22:51 +0200

    [SERVER]+- update(update serve)

--- server/middleware/uploads-multer.js
+++ server/middleware/uploads-multer.js
@@ -3,7 +3,7 @@ const multer = require('multer');
 const path = require('path');
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, '../uploads/'));
+        cb(null, path.join(__dirname, '/uploads/'));
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname);
