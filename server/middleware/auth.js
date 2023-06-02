3b13905a1b4ae4e9c7d349b29fc1caa80b271d5b
Frédéric Yaba <frederic@bobochicparIs.com>
2023-06-02 16:15:34 +0200

    [SERVER]+- update(update middleware auth.js)

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -7,6 +7,7 @@ async function authMiddleware(req, res, next) {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1];
     try {
+            console.log('Token set in auth.js: ', token)
         if (!token) {
             throw new Error("No token, authorization denied");
         }
