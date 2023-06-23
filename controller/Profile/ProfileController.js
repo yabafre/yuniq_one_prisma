9df14abb14d240d5fafd59f49cb141e2a58853d0
Frédéric Yaba <frederic@bobochicparIs.com>
2023-06-23 05:35:57 +0200

    [SERVER]+- (update profile)

--- controller/Profile/ProfileController.js
+++ controller/Profile/ProfileController.js
@@ -14,6 +14,8 @@ class ProfileController{
             // res data : user profile
            if (!userProfile) {
                throw new Error("User not found");
+           } else {
+                return res.status(200).json({message: "User profile", data: userProfile});
            }
         } catch (error) {
             return res.status(400).json({message: error.message});
